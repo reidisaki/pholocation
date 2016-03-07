e825d639759fa273b382969a25c6bb4888dea15e
Reid Isaki <reidisaki@yahoo.com>
2016-03-06 23:45:55 -0500

    code cleanup unused variables

--- Deks/app/src/main/java/com/kalei/receivers/WifiReceiver.java
+++ Deks/app/src/main/java/com/kalei/receivers/WifiReceiver.java
@@ -2,24 +2,14 @@ package com.kalei.receivers;
 
 import com.kalei.utils.PhotoLocationUtils;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-
-import java.util.List;
 
 /**
  * Created by risaki on 3/2/16.
  */
 public class WifiReceiver extends BroadcastReceiver {
-    NotificationCompat.Builder mBuilder;
-    NotificationManager mNotificationManager;
-    public static int mSuccessfulSends = 0;
-    public static int mFailedSends = 0;
-    public List<String> imageFileNames;
-    public List<String> imageFailedFileNames;
     public static final String NOTIFICATION_DELETED_ACTION = "NOTIFICATION_DELETED";
 
     @Override
--- Deks/app/src/main/java/com/kalei/services/PhotoService.java
+++ Deks/app/src/main/java/com/kalei/services/PhotoService.java
@@ -3,23 +3,13 @@ package com.kalei.services;
 import com.kalei.utils.PhotoLocationUtils;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
-import java.util.List;
 
 /**
  * Created by risaki on 3/2/16.
  */
 public class PhotoService extends IntentService {
-    NotificationCompat.Builder mBuilder;
-    NotificationManager mNotificationManager;
-    public static int mSuccessfulSends = 0;
-    public static int mFailedSends = 0;
-    public List<String> imageFileNames;
-    public List<String> imageFailedFileNames;
     public static final String NOTIFICATION_DELETED_ACTION = "NOTIFICATION_DELETED";
 
     /**
